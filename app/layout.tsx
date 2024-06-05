c46c2294466e7d47239e45972444cc3d69932501
thitipun7024 <thitipun7024@gmail.com>
2024-06-05 09:27:11 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
       <Providers>
           <SessionGuard>
             {children}
+            
           </SessionGuard>
         </Providers>
       </body>
