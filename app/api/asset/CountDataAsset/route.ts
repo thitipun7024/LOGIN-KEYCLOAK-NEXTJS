5a7ff00a59903e052eb39a4289133a6a5e4e4329
thitipun7024 <thitipun7024@gmail.com>
2024-06-26 10:37:51 +0700

    เพิ่ม การเช็คจำนวนทั้งหมดของพัสดุสาขานั้นๆ

--- app/api/asset/CountDataAsset/route.ts
+++ app/api/asset/CountDataAsset/route.ts
@@ -1,24 +1,28 @@
-import prisma from '@/lib/prisma'
-interface ResponseData{
-    count: number
-}
+import prisma from '../../../../lib/prisma'
+import { type NextRequest, NextResponse } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams
+    const BranchCodeSearch = searchParams.get("resultGroupBranch")
+
     try {
-        const count: number = await prisma.assetMaster.count({
-            where: {}
+        const count = await prisma.assetMaster.count({
+            where: {
+                BranchCode: BranchCodeSearch ?? undefined,
+            }
         });
-        if(count !== undefined){
-            const responseData: ResponseData = {
+        
+        if(count !== undefined) {
+            const responseData = {
                 count: count
             };
-            return Response.json(responseData);
+            return NextResponse.json(responseData);
         } else {
             console.error("Error: Unable to fetch SMS count");
+            return NextResponse.json({ error: 'Unable to fetch SMS count' }, { status: 500 });
         }
     } catch (error) {
-        return new Response(error as BodyInit, {
-            status: 500,
-          })
+        console.error('Error fetching count:', error);
+        return NextResponse.json({ error: 'Internal Server Error', details: error.message }, { status: 500 });
     }
 }
