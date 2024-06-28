6b022986421ae63f0234b6b18f01fed8260e72aa
thitipun7024 <thitipun7024@gmail.com>
2024-06-28 10:24:09 +0700

    เเก้่ไข การนับจำนวนสินทรัพย์ทั้งหมด เเละเปลี่ยน table ในการนับ

--- app/api/asset/CountDataAsset/route.ts
+++ app/api/asset/CountDataAsset/route.ts
@@ -4,11 +4,15 @@ import { type NextRequest, NextResponse } from 'next/server'
 export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams
     const BranchCodeSearch = searchParams.get("resultGroupBranch")
+    const SakHQCodeSearch = searchParams.get("SakHQ")
 
     try {
-        const count = await prisma.assetMaster.count({
+        const count = await prisma.no_Asset.count({
             where: {
-                BranchCode: BranchCodeSearch ?? undefined,
+                OR: [
+                    BranchCodeSearch ? { Cost_Ctr: BranchCodeSearch } : {},
+                    SakHQCodeSearch ? { Cost_Ctr: SakHQCodeSearch } : {},
+                ].filter(Boolean)
             }
         });
         
@@ -18,7 +22,7 @@ export async function GET(req: NextRequest) {
             };
             return NextResponse.json(responseData);
         } else {
-            console.error("Error: Unable to fetch SMS count");
+            console.error("Error: Unable to fetch count");
             return NextResponse.json({ error: 'Unable to fetch SMS count' }, { status: 500 });
         }
     } catch (error) {
