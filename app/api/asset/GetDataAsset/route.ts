0480b509c68baee6074bbaef154209e1d0ee737c
thitipun7024 <thitipun7024@gmail.com>
2024-06-28 10:45:43 +0700

    เพิ่มการตรวจเช็คด้วย Cost_Ctr เเละเปลี่ยนตารางในการ get ข้อมูล

--- app/api/asset/GetDataAsset/route.ts
+++ app/api/asset/GetDataAsset/route.ts
@@ -4,20 +4,33 @@ import { type NextRequest, NextResponse } from 'next/server'
 export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams
     const BranchCodeSearch = searchParams.get("resultGroupBranch")
+    const SakHQCodeSearch = searchParams.get("SakHQ")
     const Status = "16";
 
-    if (!BranchCodeSearch) {
-        return NextResponse.json({ error: 'BranchCode is required' }, { status: 400 });
+    if (!BranchCodeSearch && !SakHQCodeSearch) {
+        return NextResponse.json({ message: "ไม่มีข้อมูล" }, { status: 200 });
     }
 
     try {
-        const getBranchCode = await prisma.assetMaster.findMany({
+        const getBranchCode = await prisma.no_Asset.findMany({
             where: {
-                BranchCode: BranchCodeSearch,
-                Status: Status,
-            },
+                AND: [
+                    {
+                        OR: [
+                            BranchCodeSearch ? { Cost_Ctr: BranchCodeSearch } : undefined,
+                            SakHQCodeSearch ? { Cost_Ctr: SakHQCodeSearch } : undefined,
+                        ].filter(Boolean)
+                    },
+                    { Status: Status }
+                ]
+            }
         });
-        return NextResponse.json(getBranchCode);
+
+        if (getBranchCode.length === 0) { 
+            return NextResponse.json({ message: "ไม่มีข้อมูล" }, { status: 200 });
+        } else {
+            return NextResponse.json(getBranchCode);
+        }
     } catch (error) {
         console.error('Error fetching branch code:', error);
         return NextResponse.json({ error: 'Internal Server Error', details: error.message }, { status: 500 });
