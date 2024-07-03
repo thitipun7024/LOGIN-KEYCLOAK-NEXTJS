1dbf93b44cfb88b8748cfa957ea118eeb2458355
thitipun7024 <thitipun7024@gmail.com>
2024-07-03 16:28:56 +0700

    เพิ่ม เส่น API การนับจำนวนสินทรัพย์ที่ยังไม่ได้ตรวจ

---
+++ app/api/asset/CountDataStatusNoCheck/route.ts
@@ -0,0 +1,38 @@
+import prisma from '../../../../lib/prisma'
+import { type NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams
+    const BranchCodeSearch = searchParams.get("resultGroupBranch")
+    const SakHQCodeSearch = searchParams.get("SakHQ")
+    const Status = "16";
+
+    try {
+        const countNoChecked = await prisma.no_Asset.count({
+            where: {
+                AND:[
+                    {
+                        OR: [
+                        BranchCodeSearch ? { Cost_Ctr: BranchCodeSearch } : {},
+                        SakHQCodeSearch ? { Cost_Ctr: SakHQCodeSearch } : {},
+                    ].filter(Boolean)
+                },
+                    { Status: Status },
+                ]
+            }
+        });
+        
+        if(countNoChecked !== undefined) {
+            const responseData = {
+                countAssetNoChecked: countNoChecked
+            };
+            return NextResponse.json(responseData);
+        } else {
+            console.error("Error: No Data");
+            return NextResponse.json({ error: 'No Data' }, { status: 500 });
+        }
+    } catch (error) {
+        console.error('Error fetching count:', error);
+        return NextResponse.json({ error: 'Internal Server Error', details: error.message }, { status: 500 });
+    }
+}
