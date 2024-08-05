e50ae1e21d1c58a22b88d1d0802191a534e71f5b
oldpizza <kongecho.0@gmail.com>
2024-08-05 11:25:33 +0700

    แก้ไข api get image

--- app/api/asset/GetImageURL/route.ts
+++ app/api/asset/GetImageURL/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
-        return NextResponse.json(process.env.NEXT_PUBLIC_SMARTCARD_URI);
+        return NextResponse.json("process.env.NEXT_PUBLIC_SMARTCARD_URI");
 }
