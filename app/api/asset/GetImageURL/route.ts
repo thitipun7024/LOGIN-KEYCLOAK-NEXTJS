67b3f5ef9f2c7626825eaf730560c2c9bf7d6232
oldpizza <kongecho.0@gmail.com>
2024-08-06 11:15:20 +0700

    แก้ไขชื่อ env

--- app/api/asset/GetImageURL/route.ts
+++ app/api/asset/GetImageURL/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
 export async function GET() {
-        const a =process.env.NEXT_PUBLIC_SMARTCARD_URI
+        const a =process.env.SMARTCARD_URI
     return NextResponse.json({ uri: a });
 }
